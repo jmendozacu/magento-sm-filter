b61a9b24cf15b1e4bc8795600212fb3452e092f2
thienkimlove <thienkimlove@gmail.com>
2015-04-20 04:24:05 +0000

    add config

---
+++ app/code/local/SM/Filter/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MANHQUAN
+ * Date: 4/16/2015
+ * Time: 11:26 AM
+ */ 
+class SM_Filter_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/local/SM/Filter/sql/sm_filter_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MANHQUAN
+ * Date: 4/16/2015
+ * Time: 11:26 AM
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
